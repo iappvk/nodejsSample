35ebc223896d970047691d1884a68f783347d6f2
Vijay iMac <Vijay@Vijayakumars-iMac.local>
2015-09-07 19:39:49 +0530

    issue fixing

--- app.js
+++ app.js
@@ -6,13 +6,12 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-	var hostname = req.headers.host; // hostname = 'localhost:8080'
-  var pathname = url.parse(req.url).pathname; // pathname = '/MyApp'
-  console.log('http://' + hostname + pathname);
+	
   
   response.send('Hello World!')
 })
 
 app.listen(app.get('port'), function() {
+
   console.log("Node app is running at localhost:" + app.get('port'))
 })
