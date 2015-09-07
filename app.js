be94130a37af783359fc881a23e3e49732801909
Vijay iMac <Vijay@Vijayakumars-iMac.local>
2015-09-07 18:41:50 +0530

    issue fixing

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express')
+var url = require('url') ;
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
+	var hostname = req.headers.host; // hostname = 'localhost:8080'
+  var pathname = url.parse(req.url).pathname; // pathname = '/MyApp'
+  console.log('http://' + hostname + pathname);
+  
   response.send('Hello World!')
 })
 
