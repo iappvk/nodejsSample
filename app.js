7f00f6c7940a20d62aeb498a8ca3937bc8e62e2a
Vijay iMac <Vijay@Vijayakumars-iMac.local>
2015-09-08 17:10:23 +0530

    issue fixing

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 var express = require('express')
-var url = require('url') ;
+var url = require('url');
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.initDB = function initDB() {
-    var dbconurl = null;
-     dbconurl = 'mongodb://localhost:27017/mconnect_staging';
-    MongoClient.connect(dbconurl, function(err, database) {
-        if (err) {
-            throw err;
-        } else {
-            // db = database;
-            exports.database.connection = database;
-            console.log("Successfully connected to database");
-        }
-    });
-};
+var dbconurl = 'mongodb://localhost:27017/mconnect_staging';
+// var dbconurl = 'mongodb://182.71.230.166:27017/ProjectM_DEV'
 
+MongoClient.connect(dbconurl, function(err, database) {
+	if (err) {
+		console.log("Faild to init" + err);
+
+		throw err;
+	} else {
+		// db = database;
+		console.log("Successfully connected to database");
+		
+		var collection = db.collection('test');
+  // Find some documents 
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    assert.equal(2, docs.length);
+    console.log("Found the following records");
+    console.dir(docs);
+    callback(docs);
+  });
+  
+  
+		// exports.database.connection = database;
+
+	}
+});
 
 var app = express()
 
@@ -23,12 +36,11 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-	
-  
-  response.send('Hello World!')
+
+	response.send('Hello World!')
 })
 
 app.listen(app.get('port'), function() {
 
-  console.log("Node app is running at localhost:" + app.get('port'))
+	console.log("Node app is running at localhost:" + app.get('port'))
 })
